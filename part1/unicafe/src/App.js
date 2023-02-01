9fbdb9ec6fad14f9832d5e7327f452573b04a1c4
Danilo <29783414+RadekaD@users.noreply.github.com>
2023-02-01 21:11:37 +0100

    Complete unicafe step1 - exercise 1.6 Add good/neutral/bad button counters and display on page

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,16 +1,48 @@
 import { useState } from 'react';
 
 
+
+const Button = ( {handleClick, message} ) => {
+  return (
+    <button onClick={handleClick}>
+      {message}
+    </button>
+  )
+}
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const goodClick = () => {
+    setGood(good + 1)
+  }
+
+  const neutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const badClick = () => {
+    setBad(bad + 1)
+  }
+
+
 
   return (
     <div>
-      Yeah
+      <h1>
+        Give Feedback
+      </h1>
+      <Button handleClick={goodClick} message={"good"}/>
+      <Button handleClick={neutralClick} message={"neutral"}/>
+      <Button handleClick={badClick} message={"bad"}/>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 
