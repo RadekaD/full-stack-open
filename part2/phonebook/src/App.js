81e74e2fa1c71b0d203d7b85b34aad5ceed06a68
Danilo <29783414+RadekaD@users.noreply.github.com>
2023-02-03 11:08:24 +0100

    Complete exercise 2.9 Add filter by search functionality

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,11 +3,16 @@ import { useState } from 'react'
 const App = () => {
 
   const [ persons, setPersons ] = useState([
-    { name: "Danilo Radeka", id: "danilo-radeka", number: "063 289 7162"}
+    { name: "Danilo Radeka", id: "danilo-radeka", number: "063 289 7162"},
+    { name: "Milica Vuckovic", id: "milica-vuckovic", number: "063 289 7162"},
+    { name: "Bojana Vuckovic", id: "bojana-vuckovic", number: "063 289 7162"},
+    { name: "David Radeka", id: "david-radeka", number: "063 289 7162"},
+    { name: "Vesna Radeka", id: "vesna-radeka", number: "063 289 7162"},
+    { name: "Nenad Radeka", id: "nenad-radeka", number: "063 289 7162"},
   ])
   const [ newName, setNewName ] = useState("")
   const [ newNumber, setNewNumber ] = useState("")
-
+  const [ filteredList, setFilteredList ] = useState(persons)
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -17,9 +22,8 @@ const App = () => {
       number: newNumber
     } 
      
-
+/// Blocks ability to add already existing names by comparing id
       let keys = persons.map(person => person.id)
-
       if (keys.includes(newPerson.id)) {
         alert(`${newName} was already added to the phonebook`)
       } else {
@@ -27,11 +31,23 @@ const App = () => {
         setNewName("")
         setNewNumber("")
       }
-      
+    }
 
+    
+/// Search function
+    const filterBySearch = (e) => {
+      const query = e.target.value;
+      let updatedList = [...persons];
+      console.log(updatedList, query)
+      updatedList = updatedList.filter((item) => {
+        return item.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+      })
+
+      setFilteredList(updatedList)
     }
 
 
+/// Handles state of all input buttons
   const handleNameInput = (e) => {
     setNewName(e.target.value)
   }
@@ -43,6 +59,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          search: <input onChange={filterBySearch}  />
+        </div>
+        <h2>Add new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameInput}/>
@@ -55,10 +75,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
-        {persons.map((person) => <p key={person.id}>{person.name} {person.number}</p>)}
+      <div className='content'>
+        {filteredList.map((person) => <p key={person.id}>{person.name} {person.number}</p>)}
+        
       </div>
+      
     </div>
+    
   );
 
 }
